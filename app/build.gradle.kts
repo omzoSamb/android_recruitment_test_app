44df6dd70fae0d88d08d228e868adce0c57d03a6
papeomarsamb <papeomar.samb@bjtmail.com>
2026-02-07 23:08:42 +0100

    ✨ Feature:implementing_hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -59,6 +60,8 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.gson)
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
 
 
 
