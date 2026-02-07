d30f95fbd4db28e98c181e27c371cf9c3feb7dbe
papeomarsamb <papeomar.samb@bjtmail.com>
2026-02-07 23:08:42 +0100

    ✨ Feature:implementing_hilt

--- app/src/main/java/fr/leboncoin/androidrecruitmenttestapp/PhotoApp.kt
+++ app/src/main/java/fr/leboncoin/androidrecruitmenttestapp/PhotoApp.kt
@@ -1,9 +1,11 @@
 package fr.leboncoin.androidrecruitmenttestapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import fr.leboncoin.androidrecruitmenttestapp.di.AppDependencies
 import fr.leboncoin.androidrecruitmenttestapp.di.AppDependenciesProvider
 
+@HiltAndroidApp
 class PhotoApp : Application(), AppDependenciesProvider {
 
     override val dependencies: AppDependencies by lazy { AppDependencies(this) }
